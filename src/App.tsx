da25a26d08c54e7d55e4d64315e8667b6bd2750b
munjal patel <munjalpatel@munjal-patels-MacBook-Pro.local>
2020-09-04 14:30:29 -0400

    Refactor to use functional component:

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import axios from 'axios';
 import ReactPaginate from 'react-paginate';
-import PropTypes from 'prop-types';
+//import PropTypes from 'prop-types';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -13,64 +13,103 @@ const styles = {
   },
 };
 
-class App extends React.Component<{}, any> {
+function App() {
 
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      offset: 0,
-      data: [],
-      perPage: 6,
-      currentPage: 0,
-    };
-  }
+  // constructor(props: any) {
+  //   super(props);
+  //   this.state = {
+  //     offset: 0,
+  //     data: [],
+  //     perPage: 6,
+  //     currentPage: 0,
+  //   };
+  // }
 
-  componentDidMount = () => {
-    this.fetchData()
-  }
+  const [data, setData] = useState({ data: []});
+  const [offset, setOffset] = useState(0);
+  const [pageCount, setPageCount] = useState(0);
+  const perPage = 6;
+  const [currentPage, setCurrentPage] = useState(0);
+  const [galleryData, setGalleryData] = useState();
 
-  fetchData = () => {
+  // componentDidMount = () => {
+  //   this.fetchData()
+  // }
+  
+  useEffect(() => {
+    fetchData();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  const fetchData = async () => {
     const API_KEY = process.env.REACT_APP_CONSUMER_KEY;
-    const API_URL = 'https://api.500px.com/v1/photos?feature=popular&consumer_key=';
-    axios.get(`${API_URL}${API_KEY}`)
-      .then(res => {
-        const data = res.data;
-        const slice = data.photos.slice(this.state.offset, this.state.offset + this.state.perPage);
-
-        const galleryData = slice.map(data =>
-          <React.Fragment>
-            <div onClick={this.handleImageClick}>
-              <img src={data.image_url} alt="" />
-            </div>
-          </React.Fragment>)
-
-        this.setState({
-          pageCount: Math.ceil(data.photos.length / this.state.perPage),
-          galleryData,
-          data: data
-        })
-      });
+    const API_URL = 'https://api.500px.com/v1/photos?feature=popular&consumer_key=';  
+    const response =  await axios.get(`${API_URL}${API_KEY}`);
+    const photos = response.data.photos.slice(offset, offset + perPage);
+    
+    setData(response.data);
+    paginate(photos, response.data);
   }
 
-  handlePageClick = (e) => {
-    const selectedPage = e.selected;
-    const offset = selectedPage * this.state.perPage;
 
-    this.setState({
-      currentPage: selectedPage,
-      offset: offset
-    }, () => {
-      this.fetchData()
-    });
+  const paginate = ((arr, data) => {
+    const galleryData = arr.map(photo =>
+              <React.Fragment>
+                <div>
+                  <img src={photo.image_url} alt="" />
+                </div>
+              </React.Fragment>)
+            
+            const pageCount = Math.ceil(data.photos.length / perPage);
+
+            setPageCount(pageCount);
+            setGalleryData(galleryData);
+            // this.setState({
+            //   pageCount: Math.ceil(data.photos.length / this.state.perPage),
+            //   galleryData,
+            //   data: data
+            // })
+  });
+
+  //       const galleryData = slice.map(data =>
+  //         <React.Fragment>
+  //           <div onClick={this.handleImageClick}>
+  //             <img src={data.image_url} alt="" />
+  //           </div>
+  //         </React.Fragment>)
+
+  //       this.setState({
+  //         pageCount: Math.ceil(data.photos.length / this.state.perPage),
+  //         galleryData,
+  //         data: data
+  //       })
+  //     });
+  // }
+
+  const handlePageClick = (e) => {
+    const selectedPage = e.selected;
+    const offset = selectedPage * perPage;
+
+    setCurrentPage(selectedPage);
+    setOffset(offset);
+
+    fetchData();
+
+
+    // this.setState({
+    //   currentPage: selectedPage,
+    //   offset: offset
+    // }, () => {
+    //   this.fetchData()
+    // });
 
   };
 
-  handleImageClick = () => {
+  // handleImageClick = () => {
+  //   console.log("Photo click")
+  // }
 
-  }
-
-  render() {
-    console.log(this.state.data)
+  // render() {
+  //   console.log(this.state.data)
     return (
       <div className="App">
         <div>
@@ -84,7 +123,7 @@ class App extends React.Component<{}, any> {
             </AppBar>
           </div>
           <div className="container" >
-            {this.state.galleryData}
+            {galleryData}
           </div>
           <div className="justify">
             <ReactPaginate
@@ -92,10 +131,10 @@ class App extends React.Component<{}, any> {
               nextLabel={"Next"}
               breakLabel={"..."}
               breakClassName={"break-me"}
-              pageCount={this.state.pageCount}
+              pageCount={pageCount}
               marginPagesDisplayed={2}
               pageRangeDisplayed={5}
-              onPageChange={this.handlePageClick}
+              onPageChange={handlePageClick}
               containerClassName={"pagination"}
               activeClassName={"active"} />
           </div>
@@ -103,7 +142,5 @@ class App extends React.Component<{}, any> {
       </div>
     );
   }
-}
-
 
 export default App;
---
+++ src/components/Photo.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+type PhotoProps = {
+    data: any,
+}
+
+class Photo extends React.Component<{}, PhotoProps> {
+	constructor(props: any) {
+        super(props);
+      }
+
+	componentDidMount() {
+		
+	}
+
+	render() {
+	
+		return (
+			<div>
+				
+			</div>
+		)
+	}
+} 
+
+export default Photo;
