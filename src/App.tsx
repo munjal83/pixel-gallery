2551bd1a8c0c50db4d538a3a7f8b0ac3b2207ef8
munjal patel <munjalpatel@munjal-patels-MacBook-Pro.local>
2020-09-03 23:17:00 -0400

    add Material-Ui AppBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import './App.css';
 import axios from 'axios';
-import ReactPaginate from 'react-paginate'
+import ReactPaginate from 'react-paginate';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+};
 
 class App extends React.Component<{}, any> {
 
@@ -11,34 +20,34 @@ class App extends React.Component<{}, any> {
     this.state = {
       offset: 0,
       data: [],
-      perPage: 9,
+      perPage: 6,
       currentPage: 0,
     };
   }
 
-  componentDidMount() {
+  componentDidMount = () => {
     this.fetchData()
   }
 
-  fetchData() {
+  fetchData = () => {
     const API_KEY = process.env.REACT_APP_CONSUMER_KEY;
     const API_URL = 'https://api.500px.com/v1/photos?feature=popular&consumer_key=';
     axios.get(`${API_URL}${API_KEY}`)
       .then(res => {
         const data = res.data;
-        console.log(data);
         const slice = data.photos.slice(this.state.offset, this.state.offset + this.state.perPage);
 
-        const galleryData = slice.map(data => 
-        <React.Fragment>
-          <div>
-            <img src={data.image_url} alt="" />
-          </div>
-        </React.Fragment>)
+        const galleryData = slice.map(data =>
+          <React.Fragment>
+            <div onClick={this.handleImageClick}>
+              <img src={data.image_url} alt="" />
+            </div>
+          </React.Fragment>)
 
         this.setState({
-          pageCount: Math.ceil(data.total_items / this.state.perPage),
-          galleryData
+          pageCount: Math.ceil(data.photos.length / this.state.perPage),
+          galleryData,
+          data: data
         })
       });
   }
@@ -56,11 +65,25 @@ class App extends React.Component<{}, any> {
 
   };
 
+  handleImageClick = () => {
+
+  }
+
   render() {
+    console.log(this.state.data)
     return (
       <div className="App">
         <div>
-          <div className="container">
+          <div style={styles.root}>
+            <AppBar position="static" color="primary">
+              <Toolbar>
+                <Typography variant="title" color="inherit">
+                  Five Hundred Pixels
+                </Typography>
+              </Toolbar>
+            </AppBar>
+          </div>
+          <div className="container" >
             {this.state.galleryData}
           </div>
           <div className="justify">
